68c46b8382818360131765108c4ce8ad9805541f
emadhussein3@gmail.com <emadhussein3@gmail.com>
2023-02-04 19:50:00 +0300

    Add project files.

---
+++ WeatherApp with WPF/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Net;
+using System.Json;
+
+namespace WeatherApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            string city = CityInput.Text;
+            string apiKey = "your-api-key-here";
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    string json = client.DownloadString(url);
+                    JsonValue jsonData = JsonValue.Parse(json);
+                    string temperature = jsonData["main"]["temp"];
+                    string weatherDescription = jsonData["weather"][0]["description"];
+                    string location = jsonData["name"];
+
+                    Temperature.Content = temperature;
+                    Description.Content = weatherDescription;
+                    Location.Content = location;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not get weather data: " + ex.Message);
+                }
+            }
+        }
+    }
+}
